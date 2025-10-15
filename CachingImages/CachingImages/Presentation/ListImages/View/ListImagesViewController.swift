1714c95336512142806e5c16d68426ce7ab0301e
trung.nguyenq <nguyenquoctrung579@gmail.com>
2025-10-15 19:43:54 +0700

    Common View

--- CachingImages/CachingImages/Presentation/ListImages/View/ListImagesViewController.swift
+++ CachingImages/CachingImages/Presentation/ListImages/View/ListImagesViewController.swift
@@ -167,7 +167,7 @@ extension ListImagesViewController: UITextFieldDelegate {
             }
         }
         
-        // Debounce Search with 2s
+        // Debounce Search with 1s
         handleTextFieldChange(updatedText)
         
         return true
@@ -234,33 +234,6 @@ extension ListImagesViewController: UITableViewDelegate, UITableViewDataSource {
         // Trigger load more in 3rd last item
         if indexPath.row >= lastItem - 2 {
             self.viewModel.loadMoreImages()
-        }
-    }
-}
-// MARK: - String Extension for Emoji Detection
-extension String {
-    var containsEmoji: Bool {
-        return unicodeScalars.contains { $0.isEmoji }
-    }
-}
-extension UnicodeScalar {
-    var isEmoji: Bool {
-        switch value {
-        case 0x1F600...0x1F64F, // Emoticons
-            0x1F300...0x1F5FF, // Misc Symbols and Pictographs
-            0x1F680...0x1F6FF, // Transport and Map
-            0x1F1E6...0x1F1FF, // Regional country flags
-            0x2600...0x26FF,   // Misc symbols
-            0x2700...0x27BF,   // Dingbats
-            0xFE00...0xFE0F,   // Variation Selectors
-            0x1F900...0x1F9FF,  // Supplemental Symbols and Pictographs
-            127000...127600, // Various asian characters
-            65024...65039, // Variation selector
-            9100...9300, // Misc items
-            8400...8447: // Combining Diacritical Marks for Symbols
-            return true
-            
-        default: return false
         }
     }
 }
